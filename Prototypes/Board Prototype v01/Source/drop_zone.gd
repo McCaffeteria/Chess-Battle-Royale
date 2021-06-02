6245884911e68964a8e4e5570d2fc1c756ed5c77
Patrick McCaffrey <McCaffeteria@gmail.com>
2021-06-01 22:00:59 -0400

    Built Drag and Drop Board Prototype

---
+++ Prototypes/Board Prototype v01/Source/drop_zone.gd
@@ -0,0 +1,13 @@
+extends Position2D
+
+func _draw():
+	draw_rect(Rect2(Vector2(-32, -32), Vector2(64, 64)), Color.white)
+	#draw_circle(Vector2.ZERO, 32, Color.white)
+
+func select():
+	for child in get_tree().get_nodes_in_group("zone"):
+		child.deselect()
+	modulate = Color.red
+
+func deselect():
+	modulate = Color.white
